9c84444dfc148874dc5b34e0ab55669868a635cf
NoelEK-skol <noek07001@utb.vaxjo.se>
2024-09-25 11:18:38 +0200

    Påbörjat uppgift

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+double radie=2.4;
+Console.WriteLine("Cirkelns omkrets: "+CirkelOmkrets(radie));
+Console.WriteLine("Cirkelns area: "+ CirkelArea(radie));
+
+
+double CirkelOmkrets(double radie) {
+    return radie*2*Math.PI;
+}
+
+double CirkelArea(double radie) {
+    return Math.PI*radie*radie;
+}
+*/
+
+double BeräknaMedel(float tal1, float tal2, float tal3)
+{
+    return (tal1 + tal2 + tal3) /3;
+}
